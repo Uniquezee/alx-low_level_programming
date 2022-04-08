3345cd72e5af73d155e013f77706b84972aa3211
Uniquezee <priscaonyemaobiamarachi@gmail.com>
2022-04-07 20:34:14 -0700

    my last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+ int main(void)
+  9 {
+ 10         write(STDOUT_FILENO, "and that piece of art is useful
+ 11                         \" - Dora Korpar, 2015-10-19\n", 59);
+ 12         return (1);
+ 13 }
