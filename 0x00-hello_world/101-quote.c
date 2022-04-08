88087a8ee2a163412384640fff61e57b83969471
Uniquezee <priscaonyemaobiamarachi@gmail.com>
2022-04-07 20:46:08 -0700

    my last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
- #include <stdio.h>
-  2 #include <stdlib.h>
-  3
-  4 /**
-  5  * main - Entry point
-  6  * Return: Always 1 (Success)
-  7  */
-  8  int main(void)
-  9  {
- 10          write(STDOUT_FILENO, "and that piece of art is useful
- 11                          \" - Dora Korpar, 2015-10-19\n", 59);
- 12          return (1);
- 13  }
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
