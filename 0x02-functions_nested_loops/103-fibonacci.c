05e3a3568d6587c0815005a7730f2f451253b994
Uniquezee <priscaonyemaobiamarachi@gmail.com>
2022-04-12 21:15:45 -0700

    update 103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,26 +3,30 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * purpose - fibonacci
+ *
+ * Return: (0) always
  */
-
-int main(void)
-{
-	int i;
-	long int fibonacci[50];
-
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
-
-	for (i = 2; i < 50; i++)
+	int main(void)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if (i == 49)
-			printf("%ld\n", fibonacci[i]);
-		else
-			printf("%ld, ", fibonacci[i]);
-	}
+		unsigned long int lim = 4000000;
+		unsigned long int bef = 0;
+		unsigned long int aft = 1;
+		unsigned long int R = 0;
+		unsigned long int sum = 0;
 
-	return (0);
+		while (lim > R)
+		{
+			R = bef + aft;
+
+			if ((R % 2) == 0)
+			{
+				sum += R;
+			}
+
+			bef = aft;
+			aft = R;
+		}
+		printf("%li\n", sum);
+		return (0);
 }
